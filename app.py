ce93e8a9d0bc10e4a42914d9d03200c53550349c
Youssef Boubli <bbb.vloger@gmail.com>
2023-09-29 05:52:38 +0200

    update files and add style
    <3

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import os
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
-
-# Sample log file path
-LOG_FILE = 'logs/sample.log'
 
 # Define routes and functions
 @app.route('/')
@@ -18,18 +15,29 @@ def search():
     end_date = request.form.get('end_date')
     keyword = request.form.get('keyword')
 
+    # Get the uploaded log file
+    uploaded_file = request.files['log_file']
+
+    # Check if a file was uploaded
+    if uploaded_file:
+        # Save the uploaded file to a temporary location
+        log_file_path = 'uploads/' + uploaded_file.filename
+        uploaded_file.save(log_file_path)
+    else:
+        return "No log file uploaded."
+
     # Initialize a list to store filtered log entries
     filtered_results = []
 
     try:
         # Open and read the log file
-        with open(LOG_FILE, 'r') as log_file:
+        with open(log_file_path, 'r') as log_file:
             for line in log_file:
                 # Split the log entry into parts (assuming space-separated)
                 parts = line.strip().split(' ')
                 if len(parts) >= 3:
                     timestamp, log_level, message = parts[0], parts[1], ' '.join(parts[2:])
-                    
+
                     # Check if the timestamp falls within the date range
                     # You may need to convert timestamp to a datetime object and compare here
                     # Example: timestamp = datetime.strptime(timestamp, '%Y-%m-%d %H:%M:%S')
